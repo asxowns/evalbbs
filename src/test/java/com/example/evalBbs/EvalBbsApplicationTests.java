f8670ef011f6acab61d94e877ece0f18d3b967ff
Kim TaeJun <1000pgm@gmail.com>
2024-04-26 14:29:15 +0900

    1. 초기 설정 완료

---
+++ src/test/java/com/example/evalBbs/EvalBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evalBbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalBbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
